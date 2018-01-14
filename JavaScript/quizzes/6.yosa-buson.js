db825638031af56d08963c788303b92bc04e3254
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-15 00:14:14 +0200

    Add yosa buson quiz solution.

---
+++ JavaScript/quizzes/6.yosa-buson.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ *
+ * Build a string using concatenation by combining the lines
+ * from this famous haiku poem by Yosa Buson.
+ *
+ * "Blowing from the west
+ *  Fallen leaves gather
+ *  In the east."
+ *
+ * Each string should be printed on its own line.
+ *
+ * Hint: You will need to use special characters to produce the following output.
+ */
+
+let haiku = "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east.";
+console.log(haiku);
