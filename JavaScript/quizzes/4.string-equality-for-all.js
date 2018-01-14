a5e6b4078eb2a0edbee883e5886e04bd640df79a
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-15 00:06:11 +0200

    Add string equality for all quiz solution.

---
+++ JavaScript/quizzes/4.string-equality-for-all.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ *
+ * Fix the right side expression so it evaluates to true.
+ * "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal"
+ */
+
+// fix the right side of the expression
+let answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
