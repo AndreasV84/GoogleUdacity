ba89dbb6070537d4097989276d2b00b0118e4fce
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-14 23:57:26 +0200

    Add favorite food quiz solution.

---
+++ JavaScript/quizzes/3.favorite-food.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ *
+ * Create a string with the name of your favorite food.
+ * The first letter of the string should be capitalized.
+ */
+let favoriteFood = "Pizza";
+console.log(favoriteFood);
