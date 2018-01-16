0c548d5e91a22459a4f91b7bf722511311293cd9
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-16 09:16:24 +0200

    Add semicolons quizz solution

---
+++ JavaScript/quizzes/7.semicolons.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ *
+ * Define two variables called thingOne and thingTwo and assign them values.
+ *
+ * Print the values of both variables in one console.log statement using concatenation.
+ *
+ * Don't forget to use semicolons!
+ */
+
+// your code goes here
+let thingOne = "red ";
+let thingTwo = "blue";
+console.log(thingOne + thingTwo);
