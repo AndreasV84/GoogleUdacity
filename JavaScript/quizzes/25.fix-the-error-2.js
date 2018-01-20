0e9692a0b521e403130839449aac55b2b19677c2
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-20 23:24:13 +0200

    Add fix the error 2 quiz solution

---
+++ JavaScript/quizzes/25.fix-the-error-2.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ * for (let k = 0 k < 200 k++) {
+ * console.log(k);
+ * }
+ */
+
+// fix the for loop
+for (let k = 0; k < 200; k++) {
+    console.log(k);
+}
