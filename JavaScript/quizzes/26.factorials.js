7db244d71b1ce687818e95b46e9862b0d2922dcf
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-22 20:11:23 +0200

    Add factorials quizz solution

---
+++ JavaScript/quizzes/26.factorials.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ *
+ * Write a for loop that prints out the factorial of the number 12.
+ */
+
+// your code goes here
+
+let solution = 1;
+
+for (num = 12; num > 0; num--) {
+    solution = solution * num;
+}
+
+console.log(solution);
