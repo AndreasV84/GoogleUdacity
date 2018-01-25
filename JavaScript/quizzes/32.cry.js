9a1f854059b76d595b10be68b900c0c74f46b562
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-25 23:43:18 +0200

    Add cry quizz solution

---
+++ JavaScript/quizzes/32.cry.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ *
+ * Write a named function expression that stores
+ *  the function in a variable called cry and returns "boohoo!".
+ *
+ */
+
+// your code goes here
+let cry = function booHoo() {
+    return "boohoo!";
+};
+
+cry();
