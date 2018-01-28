795300a1d12074670b4d22eca81657308f1518f0
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-28 23:35:21 +0200

    Add joining the crew quiz solution

---
+++ JavaScript/quizzes/39.joining-the-crew.js
@@ -0,0 +1,24 @@
+/*
+ * Programming Quiz: Joining the Crew (6-6)
+ *
+ * Use the push() method to add the three new crew members to the crew array.
+ *
+ */
+
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
+
+let crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+let doctor = "Simon";
+let sister = "River";
+let shepherd = "Book";
+
+// your code goes here
+crew.push(doctor);
+crew.push(sister);
+crew.push(shepherd);
