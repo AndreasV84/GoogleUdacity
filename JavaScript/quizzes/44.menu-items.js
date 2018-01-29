fef796b45acf334c0216e52974750d2f4329aaa2
Andreas Vasilatos <and.vasilatos@gmail.com>
2018-01-29 23:51:02 +0200

    Add menu items quizz solution

---
+++ JavaScript/quizzes/44.menu-items.js
@@ -0,0 +1,16 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ *
+ * Create a breakfast object to represent the following menu item:
+ * The Lumberjack - $9.95
+ * eggs, sausage, toast, hashbrowns, pancakes
+ *
+ *
+ */
+
+// your code goes here
+let breakfast = {
+    name: 'The Lumberjack',
+    price: 9.95,
+    ingredients: ['eggs', 'sausage', 'toast', 'hashbrowns', 'pancakes']
+};
